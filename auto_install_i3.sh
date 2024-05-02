b5eb31806c6d76ce0eb80f8614e50ef06df94235
archos <archos@archoslinux.cz>
2024-05-02 07:23:30 +0200

    přidán skript pro automatickou instalaci i3 prostředí

---
+++ auto_install_i3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Zahajujeme instalaci a konfiguraci vašeho i3 desktopu."
+
+# Kontrola oprávnění
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Tento skript vyžaduje superuživatelská práva."
+  exit 1
+fi
+
+# Nastavení skriptů jako spustitelných
+echo "Nastavování skriptů jako spustitelných..."
+chmod +x *.sh
+
+# Instalace základních balíčků
+echo "Instalace základních balíčků..."
+./install_packages.sh
+
+# Instalace AUR balíčků
+echo "Instalace AUR balíčků..."
+./install_aur_package.sh
+
+# Přesun konfiguračních souborů
+echo "Aplikace konfiguračních souborů..."
+./move_config_files.sh
+
+echo "Instalace dokončena! Restartujte svůj systém pro aplikaci všech změn."
