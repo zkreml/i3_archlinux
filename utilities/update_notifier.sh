73e1071931a500e81516808e9a4e546b2cb26cff
archos <archos@archoslinux.cz>
2024-05-05 19:12:44 +0200

    do skriptu přidána notifikace pro AUR repozitář

--- utilities/update_notifier.sh
+++ utilities/update_notifier.sh
@@ -11,17 +11,21 @@
 # Toto je nutné, protože cron nezná kontext uživatelského desktopového prostředí a bez této proměnné,by aplikace typu notify-send nemohly zobrazovat notifikace na ploše.
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
-echo "Skript spuštěn v: $(date)" >> #/cest/k/log.txt 
+# Logování data spuštění
+echo "Skript spuštěn v: $(date)" >> /cesta/k/log.txt 
 
-# Získání seznamu aktualizací
-updates=$(checkupdates)
+# Získání seznamu aktualizací z oficiálních repozitářů
+official_updates=$(checkupdates)
+official_count=$(echo "$official_updates" | wc -l)
 
-# Zkontroluj, jestli jsou dostupné nějaké aktualizace
-if [[ ! -z "$updates" ]]; then
-    # Počet dostupných aktualizací
-    count=$(echo "$updates" | wc -l)
+# Získání seznamu aktualizací z AUR
+aur_updates=$(yay -Qua)
+aur_count=$(echo "$aur_updates" | wc -l)
 
-    # Zobrazení notifikace
-    notify-send "Dostupné aktualizace" "$count aktualizací je dostupných. Spusťte terminál a zadejte 'sudo pacman -Syu' pro instalaci."
+# Kontrola a zobrazení notifikací, pokud jsou dostupné aktualizace
+if [[ ! -z "$official_updates" ]] || [[ ! -z "$aur_updates" ]]; then
+    notify-send "Dostupné aktualizace" "$official_count oficiálních a $aur_count AUR aktualizací je dostupných. Spusťte terminál a zadejte 'yay -Syu' pro instalaci."
+else
+    echo "Žádné aktualizace nejsou dostupné." >> /cesta/k/log.txt
 fi
 
