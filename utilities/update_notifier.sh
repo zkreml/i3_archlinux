de2d5824f3720a4cab078b684d86a21acce037c9
archos <archos@archoslinux.cz>
2024-07-25 19:15:10 +0200

    Oprava notifikací, skript nyní zobrazuje správý počet aktualizací

--- utilities/update_notifier.sh
+++ utilities/update_notifier.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
-
 ##################################################################################################################
 # Written to be used on 64 bits computers
 # Author    :   Archos
 # Website   :   https://arch-linux.cz
 ##################################################################################################################
-##################################################################################################################
-# Nastaví DISPLAY pro GUI aplikace spouštěné z cronu
-# Nastaví proměnnou prostředí DBUS_SESSION_BUS_ADDRESS, aby bylo možné z cronu spouštět GUI aplikace.
-# Toto je nutné, protože cron nezná kontext uživatelského desktopového prostředí a bez této proměnné,by aplikace typu notify-send nemohly zobrazovat notifikace na ploše.
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
-# Logování data spuštění
-echo "Skript spuštěn v: $(date)" >> /cesta/k/log.txt 
+# Nastavení proměnných pro zobrazení notifikací
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
 # Získání seznamu aktualizací z oficiálních repozitářů
 official_updates=$(checkupdates)
-official_count=$(echo "$official_updates" | wc -l)
+official_count=$(echo "$official_updates" | grep -c "^\S")
 
 # Získání seznamu aktualizací z AUR
 aur_updates=$(yay -Qua)
-aur_count=$(echo "$aur_updates" | wc -l)
+aur_count=$(echo "$aur_updates" | grep -c "^\S")
 
 # Kontrola a zobrazení notifikací, pokud jsou dostupné aktualizace
-if [[ ! -z "$official_updates" ]] || [[ ! -z "$aur_updates" ]]; then
+if [[ $official_count -gt 0 ]] || [[ $aur_count -gt 0 ]]; then
     notify-send "Dostupné aktualizace" "$official_count oficiálních a $aur_count AUR aktualizací je dostupných. Spusťte terminál a zadejte 'yay -Syu' pro instalaci."
 else
-    echo "Žádné aktualizace nejsou dostupné." >> /cesta/k/log.txt
+    notify-send "Žádné aktualizace" "Žádné aktualizace nejsou dostupné."
 fi
 
