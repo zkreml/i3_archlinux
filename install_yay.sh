460230c69bcb3e01e0210ad067469c4cd630eec4
archos <archos@archoslinux.cz>
2025-04-02 19:39:23 +0200

    Přidány fonty do AUR skiptu

--- install_yay.sh
+++ install_yay.sh
@@ -11,25 +11,24 @@
 #
 ##################################################################################################################
 
-package_name="autotiling picom imagewriter"
+package_name="autotiling picom imagewriter ttf-openlogos-archupdate nerd-fonts-complete"
 
 # Kontrola, zda je git nainstalován
 if ! command -v git &>/dev/null; then
-    echo "Instaluji git..."
-    sudo pacman -S git --needed --noconfirm
+  echo "Instaluji git..."
+  sudo pacman -S git --needed --noconfirm
 fi
 
 # Kontrola, zda je nainstalován pomocník pro AUR (např. yay)
 if ! command -v yay &>/dev/null; then
-    echo "Instaluji Yay (AUR helper)..."
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si --noconfirm
-    cd ..
-    rm -rf yay
+  echo "Instaluji Yay (AUR helper)..."
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si --noconfirm
+  cd ..
+  rm -rf yay
 fi
 
 # Instalace balíčku z AUR pomocí Yay
 echo "Instaluji $package_name z AUR..."
 yay -S $package_name --noconfirm
-
