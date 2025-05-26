bab615652014917799631704273ea4e1fb9110c3
archos <archos@archoslinux.cz>
2025-05-26 20:05:09 +0200

    odstraněn baliček neofetch

--- pacman_packages.sh
+++ pacman_packages.sh
@@ -13,7 +13,7 @@
 # Aktualizace systému
 
 # Seznam balíčků k instalaci pomocí pacman
-pacman_packages="firefox hplip gutenprint foomatic-db vim git htop darktable geeqie smplayer fzf zsh-syntax-highlighting zsh-autosuggestions vlc thunar pcmanfm thunar-archive-plugin xclip transmission-gtk libreoffice-fresh libreoffice-fresh-cs gnome-calculator pluma p7zip unrar tar rsync cryfs rapid-photo-downloader gthumb gimp wireguard-tools  nextcloud-client  flatpak catfish totem borg borgmatic gnome-disk-utility gparted python-llfuse sshfs simple-scan evince neofetch geany gnome-screenshot filezilla networkmanager-openvpn passff-host  firefox-i18n-cs thunderbird thunderbird-i18n-cs wget yajl bitwarden cronie  noto-fonts-extra  picom pacman-contrib seahorse volumeicon neovim xdg-utils xdg-user-dirs-gtk unzip pavucontrol network-manager-applet noto-fonts ttf-dejavu file-roller"
+pacman_packages="firefox hplip gutenprint foomatic-db vim git htop darktable geeqie smplayer fzf zsh-syntax-highlighting zsh-autosuggestions vlc thunar pcmanfm thunar-archive-plugin xclip transmission-gtk libreoffice-fresh libreoffice-fresh-cs gnome-calculator pluma p7zip unrar tar rsync cryfs rapid-photo-downloader gthumb gimp wireguard-tools  nextcloud-client  flatpak catfish totem borg borgmatic gnome-disk-utility gparted python-llfuse sshfs simple-scan evince  geany gnome-screenshot filezilla networkmanager-openvpn passff-host  firefox-i18n-cs thunderbird thunderbird-i18n-cs wget yajl bitwarden cronie  noto-fonts-extra  picom pacman-contrib seahorse volumeicon neovim xdg-utils xdg-user-dirs-gtk unzip pavucontrol network-manager-applet noto-fonts ttf-dejavu file-roller"
 
 # Aktualizace systému a instalace balíčků
 echo "Aktualizuji systém a instaluji základní balíčky..."
